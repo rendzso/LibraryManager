afb112e6d589061c4aabcb8d83e5df17347232ab
rendzso <bodnarreno@gmail.com>
2019-04-26 17:39:23 +0200

    more basic exception handling added

--- src/backend/LibraryControllerStuffs.ts
+++ src/backend/LibraryControllerStuffs.ts
@@ -8,6 +8,28 @@ router.get('/list', async (req, res) => {
 });
 
 router.post('/add', (req, res) => {
+  if (req.body.type === undefined || req.body.type === '') {
+    res.status(414).send('Type is missing!');
+    return;
+  }
+  if (req.body.author === undefined || req.body.author === '') {
+    res.status(414).send('Author is missing!');
+    return;
+  }
+  if (req.body.title === undefined || req.body.title === '') {
+    res.status(414).send('Title is missing!');
+    return;
+  }
+  if (req.body.date === undefined || req.body.date === '') {
+    res.status(414).send('Date is missing!');
+    return;
+  }
+  if (req.body.stuffID === undefined || req.body.stuffID === '') {
+    res.status(414).send('StuffID is missing!');
+    return;
+  }
+  req.body.status = 'open';
+  req.body.rentID = 'none';
   srs.addNewStuff(req.body);
   res.status(200).send('Stuff added.');
 });
